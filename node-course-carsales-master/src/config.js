14671b54ee92768bb2b35c668de3efb1164b7fc8
igordamicost <82038310+igordamicost@users.noreply.github.com>
2021-08-26 19:26:23 -0300

    Update config.js

--- node-course-carsales-master/src/config.js
+++ node-course-carsales-master/src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   privateKey: '2f639018-7c65-44df-a3a2-a070bd2a10a8',
   // connectionUrlDatabase: 'mongodb://127.0.0.1:27017/car-sales',
-  connectionUrlDatabase: '',
-  apiPort: 8080,
+  connectionUrlDatabase: 'mongodb+srv://igordamicosr:kj2w52ym@calhasalesdb.mdo8e.mongodb.net/Calha-Sales?retryWrites=true&w=majority',
+  apiPort: 8082,
   corsOptions: {
     origin: ['http://127.0.0.1:4200', 'http://localhost:4200'],
   },
