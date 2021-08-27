f24744ecec2d60ae80b4b1f108a1f052661c7ded
igordamicost <82038310+igordamicost@users.noreply.github.com>
2021-08-27 10:48:21 -0300

    PrimeFLex install

--- angular-calha-page/src/app/app.module.ts
+++ angular-calha-page/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CardModule} from 'primeng/card';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
