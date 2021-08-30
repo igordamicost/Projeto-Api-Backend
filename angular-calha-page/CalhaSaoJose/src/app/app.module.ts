4fe8e68884f0f796f2ecd44d43b8389f38418159
igordamicost <82038310+igordamicost@users.noreply.github.com>
2021-08-30 18:54:59 -0300

    main

--- angular-calha-page/CalhaSaoJose/src/app/app.module.ts
+++ angular-calha-page/CalhaSaoJose/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    InputTextModule,
     AppRoutingModule
   ],
   providers: [],
